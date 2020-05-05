94dfb63fdb18d2d9956b95437cb692b0b62b308c
ume05 <yuu02out@gmail.com>
2020-05-05 15:47:45 +0900

    20を再修正

--- task.rb
+++ task.rb
@@ -255,15 +255,15 @@ class Zoo
   end
 
   def info_entry_fee(user)
-	case user.age
+	fee = case user.age
 	when 0..5 then
-		fee = @entry_fee[:infant]
+		@entry_fee[:infant]
 	when 6..12 then
-		fee = @entry_fee[:children]
+		@entry_fee[:children]
 	when 13..64 then
-		fee = @entry_fee[:adult]
+		@entry_fee[:adult]
 	else
-		fee = @entry_fee[:senior]
+		@entry_fee[:senior]
 	end
 	
 	puts "#{user.name}さんの入場料金は #{fee} 円です。"
